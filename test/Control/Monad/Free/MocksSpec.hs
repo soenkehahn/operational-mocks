04118d24652b05745b6786f36527588519dab600
Amar <amar@capital-match.com>
2016-08-07 20:13:38 +0800

    Support mocking free monads

--- test/Control/Monad/Free/MocksSpec.hs
+++ test/Control/Monad/Free/MocksSpec.hs
@@ -9,15 +9,12 @@
 
 module Control.Monad.Free.MocksSpec where
 
-import Control.Exception hiding (assert)
-import Data.Functor
-import Test.Hspec
-import Data.Maybe
-import Unsafe.Coerce
 import Control.Monad.Free
-import Control.Monad.Trans.State
-import Control.Monad.IO.Class
+import Control.Exception
+import Test.Hspec
 import Prelude hiding (getLine)
+
+import Control.Monad.Free.Mocks
 
 -- * Functor
 data ConsoleF a where
@@ -26,37 +23,25 @@ data ConsoleF a where
 
 deriving instance Functor ConsoleF
 
+instance HasMock ConsoleF where
+  mockE (GetLine f) = Right $ W f
+  mockE (WriteLine _ a) = Left a
+
+
 instance Show (ConsoleF a) where
   show (GetLine _) = "GetLine"
   show (WriteLine _ _) = "WriteLine"
 
 type Console = Free ConsoleF
 
--- instance ShowConstructor ConsoleF where
---   showConstructor (GetLine _)  = "GetLine"
---   showConstructor (WriteLine _ _) = "WriteLine"
-
-class CommandEq1 f where
-  eqC :: f a -> f b  -> Maybe ()
-
-class ShowConstructor f where
-  showConstructor :: f a -> String
-
 instance ShowConstructor ConsoleF where
   showConstructor (GetLine _) = "GetLine"
   showConstructor (WriteLine _ _) = "WriteLine"
 
-instance CommandEq1 ConsoleF where
-  eqC (GetLine _) (GetLine _) = Just ()
-  eqC (WriteLine s _) (WriteLine s2 _) = if s == s2 then Just () else Nothing
-  eqC _ _ = Nothing
-
--- instance CommandEq ConsoleF where
---   -- <fixme>
---   -- commandEq :: ConsoleF a -> ConsoleF b -> IO (Either () (a :~: b))
---   -- commandEq (GetLine _) (GetLine _) = pure $ Right Refl
---   -- commandEq (WriteLine s _) (WriteLine s1 _) = (s `shouldBe` s1) *> pure (Right Refl)
---   commandEq _ _ = pure (Left ())
+instance Assert ConsoleF where
+   assert (GetLine _) (GetLine _) = pure $ Just ()
+   assert (WriteLine s _) (WriteLine s2 _) = fmap Just (s `shouldBe` s2)
+   assert _ _ = pure Nothing
 
 -- * Primitive ops
 getLineF :: ConsoleF String
@@ -72,63 +57,43 @@ writeLine :: String -> Console ()
 writeLine = liftF . writeLineF
 
 -- * programs for testing
-getAndWrite' :: Console ()
-getAndWrite' =
-  getLine >>=
-  writeLine . reverse
-
-mock :: Mock f-> Mock f
-mock = id
-
-testFreeWithMock :: ShowConstructor f => Free f a -> Mock f -> IO ()
-testFreeWithMock freeTerms mockTerms = runStateT (foldFree with freeTerms) mockTerms >>= assertPure
-  where
-    with currentTerm = do
-          liftIO $ print (showConstructor currentTerm)
-          mocks <- get
-          case mocks of
-            [] ->  liftIO $ throwIO $ ErrorCall $ "expected: function returns, got: " ++ showConstructor currentTerm
-            (mock : rest) -> do
-              put rest
-              liftIO (assert currentTerm mock)
-
-    assertPure (_, []) = pure ()
-    assertPure (_, _) = liftIO $ throwIO $ ErrorCall $
-      "expected: call to a primitive" ++
-      ", got: function returns"
-
-
-
-type Mock f = [Mocking f]
-data Mocking f = Mocking (forall r . (forall a . (CommandEq1 f) => f a -> a -> r) -> r)
-
-returns :: CommandEq1 f => f a -> a -> Mocking f
-returns term result = Mocking (\f -> f term result)
-
-assert :: ShowConstructor f => f a -> Mocking f -> IO a
-assert fa (Mocking f) = f (\mocked returnVal ->
-                             if isJust (eqC fa mocked)
-                             then pure $ unsafeCoerce returnVal --TODO, remove unsafecoerce
-                             else throwIO $ ErrorCall $
-                                  "expected: call to " ++ showConstructor mocked ++
-                                  ", got: " ++ showConstructor fa )
-
-andThen :: Mocking f -> Mock f -> Mock f
-andThen = (:)
-
-infixr 8 `andThen`
+getAndWrite :: Console ()
+getAndWrite = getLine >>= writeLine . reverse
 
 spec :: Spec
-spec = describe "Free Mocks Specs" $
+spec = describe "Free Mocks Specs" $ do
   it "should allow to mock a sequence of operations" $ do
-    foldFree (\x -> case x of
-                 s@(GetLine f) -> putStrLn (showConstructor s) *> pure (f "Testing")
-                 p@(WriteLine s f) -> putStrLn (showConstructor p) *> putStr s *> pure f
-                 ) getAndWrite'
-
     let mockConsole = mock $
-          getLineF `returns` "YES" `andThen`
-          writeLineF "SEY" `returns` () `andThen`
+          getLineF `returns` "say" `andThen`
+          writeLineF "yas" `returns` () `andThen`
           []
+    testFreeWithMock getAndWrite mockConsole
 
-    testFreeWithMock getAndWrite' mockConsole
+  it "catch unexpected calls to primitives" $ do
+    let mockConsole = mock $
+          writeLineF "say" `returns` () `andThen`
+          writeLineF "yas" `returns` () `andThen`
+          []
+        test = testFreeWithMock getAndWrite mockConsole
+    test `shouldThrow` errorCall "expected: call to WriteLine, got: GetLine"
+
+  it "catches unexpected superfluous calls to primitives" $ do
+    let test = testFreeWithMock getAndWrite $
+          getLineF `returns` "foo" `andThen`
+          []
+    test `shouldThrow` errorCall "expected: function returns, got: WriteLine"
+
+  it "catches missing calls to primitives" $ do
+    let test = testFreeWithMock getAndWrite $
+               getLineF `returns` "foo" `andThen`
+               writeLineF "oof" `returns` () `andThen`
+               getLineF `returns` "bar" `andThen`
+               []
+    test `shouldThrow` errorCall "expected: call to a primitive, got: function returns"
+
+  it "catches wrong calls to constructors" $ do
+    let test = testFreeWithMock getAndWrite $
+               getLineF `returns` "foo" `andThen`
+               writeLineF "foo" `returns` () `andThen`
+               []
+    test `shouldThrow` (\(_ :: SomeException) -> True)
