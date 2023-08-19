179d235c891a9d5b52380509ecb9d4205e10c298
Sönke Hahn <soenkehahn@gmail.com>
2023-08-20 00:22:50 +0200

    Re-implement the library to allow do-notation in tests

--- src/Control/Monad/Operational/Mocks.hs
+++ src/Control/Monad/Operational/Mocks.hs
@@ -1,105 +1,114 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiWayIf #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Control.Monad.Operational.Mocks where
 
-import Control.Exception
-import Control.Monad.Operational
-import Data.Type.Equality
+import Control.Applicative
+import Control.Monad.Operational qualified as Op
+import Data.Maybe
+import Data.Typeable
 import Test.Hspec
 import Prelude hiding (gcd, log)
 
-data Mock primitive a where
-  AndThen :: MockedPrimitive primitive a -> Mock primitive b -> Mock primitive b
-  TestResult :: (a -> IO ()) -> Mock primitive a
+data CommandCheck command a = CommandCheck
+  { name :: Maybe String,
+    check :: (forall other. command other -> IO (Either () (a :~: other)))
+  }
 
-testResult :: (a -> IO ()) -> Mock primitive a
-testResult = TestResult
+data Mock command a where
+  Cons ::
+    CommandCheck command tmp ->
+    tmp ->
+    Mock command b ->
+    Mock command b
+  Nil :: a -> Mock command a
 
-result :: (Show a, Eq a) => a -> Mock primitive a
-result mock = TestResult $ \real ->
-  real `shouldBe` mock
+instance Functor (Mock command) where
+  fmap :: (a -> b) -> Mock command a -> Mock command b
+  fmap f = \case
+    Cons commandCheck tmp next -> Cons commandCheck tmp (fmap f next)
+    Nil a -> Nil $ f a
 
-andThen :: MockedPrimitive primitive a -> Mock primitive b -> Mock primitive b
-andThen = AndThen
+instance Applicative (Mock command) where
+  pure :: a -> Mock command a
+  pure = Nil
 
-infixr 8 `andThen`
+  (<*>) :: Mock command (a -> b) -> Mock command a -> Mock command b
+  a <*> b = a >>= (\f -> fmap f b)
 
-data MockedPrimitive primitive a where
-  TestPrimitive ::
-    (forall a. primitive a -> IO (a :~: a')) ->
-    a' ->
-    MockedPrimitive primitive a'
+instance Monad (Mock command) where
+  (>>=) :: Mock command a -> (a -> Mock command b) -> Mock command b
+  a >>= b = case a of
+    Cons commandCheck tmp rest -> Cons commandCheck tmp (rest >>= b)
+    Nil tmp -> b tmp
 
-testPrimitive ::
-  (forall a. primitive a -> IO (a :~: a')) ->
-  a' ->
-  MockedPrimitive primitive a'
-testPrimitive = TestPrimitive
-
-testWithMock ::
-  (Show a, Eq a, ShowConstructor primitive) =>
-  Program primitive a ->
-  Mock primitive a ->
-  IO ()
-testWithMock real mock = case (view real, mock) of
-  (realCommand :>>= nextProgram, TestPrimitive predicate mockResult `AndThen` nextMock) -> do
-    refl <- predicate realCommand
-    testWithMock (nextProgram (castWith (sym refl) mockResult)) nextMock
-  (Return realResult, TestResult predicate) -> do
-    predicate realResult
-  (Return _, _ `AndThen` _) ->
-    throwIO $
-      ErrorCall $
-        "expected: call to a primitive"
-          ++ ", got: function returns"
-  (realCommand :>>= _, TestResult _) ->
-    throwIO $
-      ErrorCall $
-        "expected: function returns, got: " ++ showConstructor realCommand
-
--- * convenience
-
-andThen_ ::
-  (ShowConstructor primitive, CommandEq primitive) =>
-  primitive () ->
-  Mock primitive a ->
-  Mock primitive a
-prim `andThen_` next = prim `returns` () `andThen` next
-
-infixr 8 `andThen_`
+commandSatisfies ::
+  (forall other. command other -> IO (Either () (a :~: other))) ->
+  a ->
+  Mock command ()
+commandSatisfies check a =
+  Cons (CommandCheck {name = Nothing, check}) a (Nil ())
 
 returns ::
-  forall mockResult primitive.
-  (CommandEq primitive, ShowConstructor primitive) =>
-  primitive mockResult ->
-  mockResult ->
-  MockedPrimitive primitive mockResult
-returns mockPrimitive = testPrimitive p
-  where
-    p :: primitive realResult -> IO (realResult :~: mockResult)
-    p realPrimitive = do
-      testResult <- commandEq realPrimitive mockPrimitive
-      case testResult of
-        Left () ->
-          throwIO $
-            ErrorCall $
-              "expected: call to "
-                ++ showConstructor mockPrimitive
-                ++ ", got: "
-                ++ showConstructor realPrimitive
-        Right refl -> do
-          return refl
+  (CommandEq command) =>
+  command a ->
+  a ->
+  Mock command ()
+returns expected a =
+  Cons
+    (CommandCheck (Just $ showConstructor expected) (commandEq expected))
+    a
+    (Nil ())
 
--- * CommandEq
+testWithMock ::
+  (Show a, Eq a, CommandEq command) =>
+  Op.Program command a ->
+  Mock command a ->
+  IO ()
+testWithMock real mock =
+  go (Op.view real) mock
+  where
+    go ::
+      (Show a, Eq a, CommandEq command) =>
+      Op.ProgramView command a ->
+      Mock command a ->
+      IO ()
+    go real mock =
+      case (real, mock) of
+        (realCommand Op.:>>= realRest, Cons commandCheck tmp mockRest) -> do
+          result <- check commandCheck realCommand
+          case result of
+            Left () ->
+              error $ case name commandCheck of
+                Just name ->
+                  "expected: call to "
+                    <> name
+                    <> ", got: "
+                    <> showConstructor realCommand
+                Nothing -> "predicate failed on: " <> showConstructor realCommand
+            Right Refl -> do
+              go (Op.view (realRest tmp)) mockRest
+        (Op.Return real, Nil mocked) -> do
+          real `shouldBe` mocked
+        (realCommand Op.:>>= _, Nil _) ->
+          error $
+            "expected: function returns, got: "
+              <> showConstructor realCommand
+        (Op.Return _, Cons commandCheck _ _) ->
+          error $
+            case name commandCheck of
+              Just name ->
+                "expected: call to "
+                  <> name
+                  <> ", got: function returns"
+              Nothing -> "expected: call to another command, got: function returns"
 
 class CommandEq command where
-  commandEq :: command a -> command b -> IO (Either () (a :~: b))
+  commandEq :: command a -> (forall other. command other -> IO (Either () (a :~: other)))
 
-class ShowConstructor f where
-  showConstructor :: f a -> String
+  showConstructor :: command a -> String
--- test/Control/Monad/Operational/MocksSpec.hs
+++ test/Control/Monad/Operational/MocksSpec.hs
@@ -1,73 +1,152 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Control.Monad.Operational.MocksSpec where
 
-import Control.Exception
+import Control.Applicative
+import Control.Exception (ErrorCall (ErrorCall), Exception, catch, throwIO)
+import Control.Monad
 import Control.Monad.Operational
 import Control.Monad.Operational.Mocks
 import Data.Typeable
+import Test.HUnit.Lang
 import Test.Hspec
 import Prelude hiding (getLine)
 
 spec :: Spec
 spec = do
+  describe "viewMock" $ do
+    it "converts single mocked commands" $ do
+      let mock = do
+            GetLine `returns` "foo"
+      case mock of
+        Cons (CommandCheck (Just "GetLine") _) _ (Nil ()) -> return ()
+        _ -> fail "error" :: IO ()
+
+    it "converts single return statements" $ do
+      let viewed = do
+            return "foo"
+      case viewed of
+        Nil "foo" -> return ()
+        _ -> fail "error" :: IO ()
+
+    it "converts multiple commands" $ do
+      let viewed = do
+            GetLine `returns` "foo"
+            WriteLine "bar" `returns` ()
+            GetLine `returns` "baz"
+      case viewed of
+        Cons (CommandCheck (Just "GetLine") _) _ (Cons (CommandCheck (Just "WriteLine") _) _ _) -> return ()
+        _ -> fail "error" :: IO ()
+
+    it "normalizes binds" $ do
+      let viewed = do
+            (GetLine `returns` "foo" >> WriteLine "bar" `returns` ())
+            GetLine `returns` "baz"
+      case viewed of
+        Cons (CommandCheck (Just "GetLine") _) _ (Cons (CommandCheck (Just "WriteLine") _) _ _) -> return ()
+        _ -> fail "error" :: IO ()
+
   describe "testWithMock" $ do
-    it "allows to test against a sequence of primitive operations" $ do
-      testWithMock lineReverse $
+    it "allows to test against a sequence of command operations" $ do
+      testWithMock lineReverse $ do
         GetLine `returns` "foo"
-          `andThen` WriteLine "oof"
-            `andThen_` result ()
+        WriteLine "oof" `returns` ()
 
-    it "catches unexpected primitive calls" $ do
+    it "catches unexpected command calls" $ do
       let test =
-            testWithMock lineReverse $
+            testWithMock lineReverse $ do
               GetLine `returns` "foo"
-                `andThen` GetLine `returns` "foo"
-                `andThen` result ()
-      test `shouldThrow` errorCall "expected: call to GetLine, got: WriteLine"
+              GetLine `returns` "foo"
+              return ()
+      test `shouldError` "expected: call to GetLine, got: WriteLine"
 
-    it "catches unexpected superfluous calls to primitives" $ do
+    it "catches unexpected superfluous calls to commands" $ do
+      let test = testWithMock lineReverse $ do
+            GetLine `returns` "foo"
+            return ()
+      test `shouldError` "expected: function returns, got: WriteLine"
+
+    it "catches missing calls to commands" $ do
       let test =
-            testWithMock lineReverse $
+            testWithMock lineReverse $ do
               GetLine `returns` "foo"
-                `andThen` result ()
-      test `shouldThrow` errorCall "expected: function returns, got: WriteLine"
+              WriteLine "oof" `returns` ()
+              GetLine `returns` "bar"
+              return ()
+      test `shouldError` "expected: call to GetLine, got: function returns"
 
-    it "catches missing calls to primitives" $ do
+    it "catches wrong results" $ do
       let test =
-            testWithMock lineReverse $
+            testWithMock (lineReverse >> return True) $ do
               GetLine `returns` "foo"
-                `andThen` WriteLine "oof"
-                  `andThen_` GetLine
-                  `returns` "bar"
-                `andThen` result ()
-      test `shouldThrow` errorCall "expected: call to a primitive, got: function returns"
+              WriteLine "oof" `returns` ()
+              return False
+      HUnitFailure _ e :: HUnitFailure <- expectError test
+      formatFailureReason e `shouldBe` "expected: False\n but got: True"
 
-    it "allows to test a primitive like Fork" $ do
-      let forkedOutputMock :: TestPrimitive a -> IO (a :~: ())
-          forkedOutputMock = \case
+    it "has a Functor implementation" $ do
+      testWithMock lineReverse $ do
+        expected <- fmap reverse (GetLine `returns` "foo" >> return "foo")
+        WriteLine expected `returns` ()
+
+    it "has an Applicative implementation" $ do
+      let testProgram = do
+            output <- liftA2 (<>) getLine getLine
+            writeLine output
+      testWithMock testProgram $ do
+        result <-
+          (GetLine `returns` "foo" >> return reverse)
+            <*> (GetLine `returns` "bar" >> return "bar")
+        WriteLine ("foo" <> reverse result) `returns` ()
+
+    it "allows to use predicates for arguments given to commands" $ do
+      let predicate :: TestCommand other -> IO (Either () (() :~: other))
+          predicate = \case
+            WriteLine line -> do
+              length line `shouldBe` 3
+              return $ Right Refl
+            _ -> return $ Left ()
+      testWithMock lineReverse $ do
+        GetLine `returns` "foo"
+        commandSatisfies predicate ()
+      let failing = testWithMock lineReverse $ do
+            commandSatisfies predicate ()
+      failing `shouldError` "predicate failed on: GetLine"
+      let failing = testWithMock lineReverse $ do
+            GetLine `returns` "foo"
+            WriteLine "oof" `returns` ()
+            commandSatisfies predicate ()
+      failing `shouldError` "expected: call to another command, got: function returns"
+
+    it "allows to test a command like Fork" $ do
+      let forkedOutput :: TestProgram ()
+          forkedOutput = do
+            fork $ do
+              writeLine "forked"
+            writeLine "not forked"
+      let checkForkedProgram :: TestCommand a -> IO (Either () (() :~: a))
+          checkForkedProgram = \case
             Fork forked -> do
-              let mock = (WriteLine "forked" `andThen_` result ())
-              testWithMock forked mock
-              return Refl
+              testWithMock forked $ do
+                WriteLine "forked" `returns` ()
+              return $ Right Refl
             _ -> throwIO $ ErrorCall "expected: Fork"
-      testWithMock forkedOutput $
-        testPrimitive forkedOutputMock ()
-          `andThen` WriteLine "not forked"
-            `andThen_` result ()
+      testWithMock forkedOutput $ do
+        commandSatisfies checkForkedProgram ()
+        WriteLine "not forked" `returns` ()
 
--- * primitives
+-- * commands
 
-type TestProgram = Program TestPrimitive
+type TestProgram = Program TestCommand
 
-data TestPrimitive a where
-  GetLine :: TestPrimitive String
-  WriteLine :: String -> TestPrimitive ()
-  Fork :: TestProgram () -> TestPrimitive ()
+data TestCommand a where
+  GetLine :: TestCommand String
+  WriteLine :: String -> TestCommand ()
+  Fork :: TestProgram () -> TestCommand ()
 
 getLine :: TestProgram String
 getLine = singleton GetLine
@@ -78,20 +157,16 @@ writeLine = singleton . WriteLine
 fork :: TestProgram () -> TestProgram ()
 fork = singleton . Fork
 
-instance CommandEq TestPrimitive where
+instance CommandEq TestCommand where
   commandEq GetLine GetLine = return $ Right Refl
   commandEq (WriteLine a) (WriteLine b) = do
     a `shouldBe` b
     return $ Right Refl
-  commandEq (Fork _) (Fork _) = do
-    error "can't test forks like this"
   commandEq _ _ = return $ Left ()
 
-instance ShowConstructor TestPrimitive where
   showConstructor = \case
     GetLine -> "GetLine"
     WriteLine _ -> "WriteLine"
-    Fork _ -> "Fork"
 
 -- * test programs
 
@@ -100,8 +175,14 @@ lineReverse = do
   l <- getLine
   writeLine (reverse l)
 
-forkedOutput :: TestProgram ()
-forkedOutput = do
-  fork $ do
-    writeLine "forked"
-  writeLine "not forked"
+-- * test helpers
+
+shouldError :: IO () -> String -> IO ()
+shouldError test message = do
+  ErrorCall m <- expectError test
+  m `shouldBe` message
+
+expectError :: (Exception e) => IO () -> IO e
+expectError test =
+  (test >> expectationFailure "expectError: no exception thrown" >> error "impossible")
+    `catch` return
